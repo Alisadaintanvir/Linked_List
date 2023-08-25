b9e4e9fec3ec2951bb120dbea3419d86e89224fa
alisadaintanvir <alisadaintanvir@gmail.com>
2023-08-26 02:06:15 +0600

    add insert feature

--- main.py
+++ main.py
@@ -75,11 +75,45 @@ class LinkedList:
             self.tail = None
         return temp
 
+    def get(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        temp = self.head
+        for _ in range(index):
+            temp = temp.next
+        return temp
 
-my_linked_list = LinkedList(2)
-my_linked_list.append(3)
-my_linked_list.append(4)
-my_linked_list.pop()
-my_linked_list.prepend(1)
-my_linked_list.pop_first()
+    def set_value(self, index, value):
+        temp = self.get(index)
+        if temp:
+            temp.value = value
+            return True
+        return False
+
+    def insert(self, index, value):
+        if index < 0 or index > self.length:
+            return False
+        if index == 0:
+            return self.prepend(value)
+        if index == self.length:
+            return self.append(value)
+
+        new_node = Node(value)
+        temp = self.get(index-1)
+        new_node.next = temp.next
+        temp.next = new_node
+        self.length += 1
+        return True
+
+
+my_linked_list = LinkedList(0)
+my_linked_list.append(2)
+# my_linked_list.append(4)
+# my_linked_list.append(5)
+# my_linked_list.pop()
+# my_linked_list.prepend(1)
+# my_linked_list.pop_first()
+# my_linked_list.set_value(1, 5)
+# my_linked_list.insert(0, 1)
 my_linked_list.print_list()
+# print(my_linked_list.get(0))
